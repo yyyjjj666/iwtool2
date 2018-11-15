a9920804c1352582eef1c0a6e74fea9d8c21cffc
2581870602@qq.com <youjun1991@163.com>
2018-11-15 16:41:14 +0800

    1.3.0

--- test/test.js
+++ test/test.js
@@ -1,16 +1,5 @@
 let tool = require('../src/index');
 
 
-let mysql = new tool.mysqlHelper({
-    username: 'edu',
-    password: 'edu_123456',
-    database: 'edu_system',
-    host: "47.92.91.50",
-    port: '700',
-    dialect: 'mysql',
-}, null);
-
-let a=(id) => { return mysql.Query(`SELECT * FROM \`app_user\` WHERE id='${id}'`);};
- a(1);
 
 
